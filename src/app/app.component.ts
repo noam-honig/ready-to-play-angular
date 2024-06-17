7b0c046c45d038cd061c6252472dde594c6043db
Noam Honig <noam.honig@gmail.com>
2024-06-17 10:08:51 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Task } from '../shared/model';
+import { repo } from 'remult';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ready-to-play-angular';
+  tasks: Task[] = [];
+  ngOnInit() {
+    repo(Task)
+      .find()
+      .then((x) => (this.tasks = x));
+  }
 }

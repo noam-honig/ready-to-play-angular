9d3c24c178df1a146f0260bc065b36c7f082ce24
Noam Honig <noam.honig@gmail.com>
2024-06-17 10:35:46 +0300

    lets try

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Task } from '../shared/model';
+import { Task } from '../../../shared/model';
 import { repo } from 'remult';
 
 @Component({
